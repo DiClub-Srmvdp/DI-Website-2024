816a35012ffd082475838b6aa3569355472f92f0
A-R-J-U <shirazdxb.ss@gmail.com>
2025-01-24 00:00:32 +0530

    ScrollBar

--- src/pages/Hexcore.jsx
+++ src/pages/Hexcore.jsx
@@ -29,10 +29,10 @@ const Hexcore = () => {
     
     <div className="HS_page">
         <div className=" HS-main container-fluid p-5" style={ bg_style('Hex.png') }>
-            <div className=" HS-wrap d-flex flex-column align-items-center justify-content-center w-100 h-100" style={{ backdropFilter: 'blur(2px)' }} >
+            <div className=" p-3 HS-wrap d-flex flex-column align-items-center justify-content-center w-100 h-100" style={{ backdropFilter: 'blur(2px)' }} >
 
                 <h1 className='fw-semibold font-Arcane' style={{ fontSize: '7em',letterSpacing:'4px'}}>HEXCORE</h1>
-                <p className='fw-small fs-6 '>
+                <p className='fw-small fs-6  '>
                     m is not simply random text. It has roots in a piece of classical  of Good and Evil by Cicero, written in 45 BC. 
                 </p>
                 <button className='rounded-4 fs-5 text-white bg-black border border-1 border-gray-300 ' style={{ height:'3.5rem', width:'9.5rem' , backgroundImage:'linear-gradient(110deg, #9d7b30 0%, #020201 50%, transparent 50%, transparent 100%)'}} > 
