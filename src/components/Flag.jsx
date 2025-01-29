a11c497a061581c26ddda56cf142362086ce19d8
A-R-J-U <shirazdxb.ss@gmail.com>
2025-01-29 13:39:03 +0530

    Touch up

--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -7,8 +7,8 @@ const Flag = () => {
   return (
     <div className="flg">
         <Link to='/Hexcore'>
-            <img src={ flag } alt="Hexcore" className='w-flag position-fixed bottom-0 end-0 mb-4 mx-4' />
-            <img src={ logo } alt="Logo" className='w-flag position-fixed bottom-0 end-0 mx-4 mb-4 p-4'/>
+            <img src={ flag } alt="Hexcore" className='w-flag position-fixed bottom-0 end-0 mb-4 mx-4'/>
+            <img src={ logo } alt="Logo" className='w-flag position-fixed bottom-0 end-0 mx-4 mb-4 p-3'/>
         </Link>      
     </div>
   )
